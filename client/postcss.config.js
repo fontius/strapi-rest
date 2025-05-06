500b86366cadaf8841aac01248859199c392e3f2
fontius <fontius@gmail.com>
2025-05-06 07:37:54 -0400

    Clean up repo: remove node_modules and update config files

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,7 @@
+// filepath: c:\Users\Kenf\Documents\content-work\clients\02-25\strapi-rest\client\postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
